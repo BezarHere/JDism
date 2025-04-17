002830c825ca6214c48817959cfd0a52d4e2b9f9
botatodev <botatodev@gmail.com>
2025-04-17 20:40:45 +0300

    attributes refactor

--- src/jdism/Attribute.cs
+++ src/jdism/Attribute.cs
@@ -1,83 +1,198 @@
+using System.Diagnostics;
+using System.Reflection;
+
 namespace JDism;
 
-public enum AttributeType : ushort
+public enum JAttributeType : ushort
 {
-	None = 0,
-	ConstantValue,
-	Code,
-	StackMapTable,
-	Exceptions,
-	BootstrapMethods,
-	InnerClasses,
-	EnclosingMethod,
-	Synthetic,
-	Signature,
-	RuntimeVisibleAnnotations,
-	RuntimeInvisibleAnnotations,
-	RuntimeVisibleParameterAnnotations,
-	RuntimeInvisibleParameterAnnotations,
-	RuntimeVisibleTypeAnnotations,
-	RuntimeInvisibleTypeAnnotations,
-	AnnotationDefault,
-	MethodParameters,
-	SourceFile,
-	SourceDebugExtension,
-	LineNumberTable,
-	LocalVariableTable,
-	LocalVariableTypeTable,
-	Deprecated,
-	UserDefined = 0xffff,
+  None = 0,
+  ConstantValue,
+  Code,
+  StackMapTable,
+  Exceptions,
+  BootstrapMethods,
+  InnerClasses,
+  EnclosingMethod,
+  Synthetic,
+  Signature,
+  RuntimeVisibleAnnotations,
+  RuntimeInvisibleAnnotations,
+  RuntimeVisibleParameterAnnotations,
+  RuntimeInvisibleParameterAnnotations,
+  RuntimeVisibleTypeAnnotations,
+  RuntimeInvisibleTypeAnnotations,
+  AnnotationDefault,
+  MethodParameters,
+  SourceFile,
+  SourceDebugExtension,
+  LineNumberTable,
+  LocalVariableTable,
+  LocalVariableTypeTable,
+  Deprecated,
+  UserDefined = 0xffff,
 }
 
-public class Attribute
+public abstract class JAttribute
 {
-	public struct ConstantValueInfo
-	{
-		public ushort Index;
-	}
-	public struct SignatureInfo
-	{
-		public ushort Index;
-		public JType Value;
-	}
-	public record ExceptionRecord(ushort StartPc, ushort EndPc, ushort HandlerPc, ushort CatchType);
+  public record AttributeTypeInfo(Type InstanceType, JAttributeType AttributeType, string Name)
+  {
+    public AttributeTypeInfo(Type Type, JAttributeType AttributeType)
+      : this(Type, AttributeType, AttributeType.ToString())
+    {
+    }
+  }
 
-	public struct CodeInfo
-	{
+  public static readonly AttributeTypeInfo[] AttributeTypeInfos = [
+    new(typeof(ConstantValueInfoJAttribute), JAttributeType.ConstantValue),
+    new(typeof(SignatureJAttribute), JAttributeType.Signature),
+    new(typeof(CodeInfoJAttribute), JAttributeType.Code),
+    new(typeof(StackMapFrameInfoJAttribute), JAttributeType.StackMapTable),
 
-		public ushort MaxStack;
-		public ushort MaxLocals;
-		public Instruction[] Instructions;
+    new(typeof(UnknownJAttribute), JAttributeType.Exceptions),
+    new(typeof(UnknownJAttribute), JAttributeType.BootstrapMethods),
+    new(typeof(UnknownJAttribute), JAttributeType.InnerClasses),
+    new(typeof(UnknownJAttribute), JAttributeType.EnclosingMethod),
+    new(typeof(UnknownJAttribute), JAttributeType.Synthetic),
+    new(typeof(UnknownJAttribute), JAttributeType.Signature),
+    new(typeof(UnknownJAttribute), JAttributeType.RuntimeVisibleAnnotations),
+    new(typeof(UnknownJAttribute), JAttributeType.RuntimeInvisibleAnnotations),
+    new(typeof(UnknownJAttribute), JAttributeType.RuntimeVisibleParameterAnnotations),
+    new(typeof(UnknownJAttribute), JAttributeType.RuntimeInvisibleParameterAnnotations),
+    new(typeof(UnknownJAttribute), JAttributeType.RuntimeVisibleTypeAnnotations),
+    new(typeof(UnknownJAttribute), JAttributeType.RuntimeInvisibleTypeAnnotations),
+    new(typeof(UnknownJAttribute), JAttributeType.AnnotationDefault),
+    new(typeof(UnknownJAttribute), JAttributeType.MethodParameters),
+    new(typeof(UnknownJAttribute), JAttributeType.SourceFile),
+    new(typeof(UnknownJAttribute), JAttributeType.SourceDebugExtension),
+    new(typeof(UnknownJAttribute), JAttributeType.LineNumberTable),
+    new(typeof(UnknownJAttribute), JAttributeType.LocalVariableTable),
+    new(typeof(UnknownJAttribute), JAttributeType.LocalVariableTypeTable),
+    new(typeof(UnknownJAttribute), JAttributeType.Deprecated),
 
-		public ExceptionRecord[] ExceptionRecords;
-		public Attribute[] Attributes;
-	}
+  ];
+  public static readonly AttributeTypeInfo CustomAttributeType =
+    new(typeof(CustomJAttribute), JAttributeType.UserDefined);
 
-	public record VerificationTypeRecord(byte Type, ushort Index);
-
-	public struct StackMapFrameInfo
-	{
-		public byte Type = 255;
-		public ushort OffsetDelta = 0;
-		public VerificationTypeRecord[] Locals = [];
-		public VerificationTypeRecord[] Stack = [];
-
-		public StackMapFrameInfo()
-		{
-		}
-	}
-
-	public AttributeType Type;
-	public ushort NameIndex;
-	public string Name = "";
+  public AttributeTypeInfo GetTypeInfo() => FetchAttributeTypeInfo(GetType());
 
 
+  public static AttributeTypeInfo FetchAttributeTypeInfo(string name)
+  {
+    return AttributeTypeInfos.FirstOrDefault(
+      attr_info => attr_info.Name == name,
+      CustomAttributeType
+    );
+  }
 
-	public ConstantValueInfo ConstantValue;
-	public SignatureInfo Signature;
-	public CodeInfo Code;
-	public StackMapFrameInfo[] StackMapTable = [];
+  public static AttributeTypeInfo FetchAttributeTypeInfo(JAttributeType type)
+  {
+    return AttributeTypeInfos.FirstOrDefault(
+      attr_info => attr_info.AttributeType == type,
+      CustomAttributeType
+    );
+  }
 
-	// for custom defined attributes
-	public byte[] _data = [];
+  public static AttributeTypeInfo FetchAttributeTypeInfo(Type type)
+  {
+    Debug.Assert(type.IsAssignableTo(typeof(JAttribute)));
+
+    return AttributeTypeInfos.FirstOrDefault(
+      attr_info => attr_info.InstanceType == type,
+      CustomAttributeType
+    );
+  }
+
+  public static AttributeTypeInfo FetchAttributeTypeInfo<T>() where T : JAttribute
+  {
+    return AttributeTypeInfos.FirstOrDefault(
+      attr_info => attr_info.InstanceType == typeof(T),
+      CustomAttributeType
+    );
+  }
+
+
+
+
 }
+
+public class ConstantValueInfoJAttribute : JAttribute
+{
+  public Constant Constant;
+  public ushort Index;
+
+
+  public override string ToString()
+  {
+    return $"@Constant({Constant})";
+  }
+}
+
+public class SignatureJAttribute : JAttribute
+{
+  public ushort Index;
+  public JType Signature;
+
+  public override string ToString()
+  {
+    return $"@Signature({Signature})";
+  }
+}
+
+public record ExceptionRecord(ushort StartPc, ushort EndPc, ushort HandlerPc, ushort CatchType);
+
+public class CodeInfoJAttribute : JAttribute
+{
+
+  public ushort MaxStack;
+  public ushort MaxLocals;
+  public Instruction[] Instructions;
+
+  public ExceptionRecord[] ExceptionRecords;
+  public Attribute[] Attributes;
+
+  public override string ToString()
+  {
+    string instructions_str = string.Join(", ", Instructions);
+    return $"locals={MaxLocals}, stack_size={MaxStack}, code=[{instructions_str}]";
+  }
+
+}
+
+public record VerificationTypeRecord(byte Type, ushort Index);
+
+public class StackMapFrameInfoJAttribute : JAttribute
+{
+  public byte Type = 255;
+  public ushort OffsetDelta = 0;
+  public VerificationTypeRecord[] Locals = [];
+  public VerificationTypeRecord[] Stack = [];
+
+  public StackMapFrameInfoJAttribute()
+  {
+  }
+
+}
+
+public class UnknownJAttribute(JAttributeType type, byte[] data) : JAttribute
+{
+  public JAttributeType Type => type;
+  public byte[] Data => data;
+
+  public override string ToString()
+  {
+    string data_str = string.Join(", ", from i in Data select i.ToString("X"));
+    return $"@{Type}({data_str})";
+  }
+}
+
+public class CustomJAttribute(byte[] data) : JAttribute
+{
+  public byte[] Data => data;
+
+  public override string ToString()
+  {
+    string data_str = string.Join(", ", from i in Data select i.ToString("X"));
+    return $"@CustomAttribute({data_str})";
+  }
+}
+
