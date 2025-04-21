fe2ad88582e50a4ecf9a38dba43faa0368043a51
botatodev <botatodev@gmail.com>
2025-04-21 18:53:48 +0300

    more changes to the bytesource class

--- src/util/ByteSource.cs
+++ src/util/ByteSource.cs
@@ -2,24 +2,26 @@
 
 ref struct ByteSource
 {
-  public readonly ReadOnlySpan<byte> Source { get; init; }
+  public readonly ReadOnlySpan<byte> Content { get; init; }
   public readonly Endianness Endianness { get; init; } = Endianness.Big;
   public int Position { get; private set; } = 0;
 
-  public readonly bool Good => Position >= 0 && Position < Source.Length;
+  public readonly bool Good => Position >= 0 && Position < Content.Length;
+
+  public readonly bool Depleted => Position >= Content.Length;
 
   public readonly byte this[int index] {
-    get => Source[Position + index];
+    get => Content[Position + index];
   }
 
   public ByteSource()
   {
-    Source = [];
+    Content = [];
   }
 
   public ByteSource(ReadOnlySpan<byte> source, Endianness endianness = Endianness.Big)
   {
-    Source = source;
+    Content = source;
     Endianness = endianness;
   }
 
@@ -34,7 +36,7 @@ ref struct ByteSource
         Position += pos;
         return;
       case SeekOrigin.End:
-        Position = Source.Length - pos;
+        Position = Content.Length - pos;
         return;
     }
   }
@@ -43,80 +45,86 @@ ref struct ByteSource
   {
     Position = Translate(index);
   }
-
-  public readonly ReadOnlySpan<byte> Get(Range range)
+  
+  public readonly ByteSource Slice(Range range)
   {
     (int start, int end) = Translate(range);
-    return Get(start, end - start);
+    return Slice(start, end - start);
   }
 
-  public readonly ReadOnlySpan<byte> Get(int start, int length)
+  public readonly ByteSource Slice(int start, int length)
   {
-    return Source.Slice(start, length);
+    return new (Content.Slice(start, length), Endianness);
+  }
+
+  public ReadOnlySpan<byte> Get(int length)
+  {
+    Position += length;
+    return Content.Slice(Position - length, length);
   }
 
   public byte GetByte()
   {
-    return Source[Position++];
+    return Content[Position++];
   }
 
   public sbyte GetSByte()
   {
-    return (sbyte)Source[Position++];
+    return (sbyte)Content[Position++];
   }
 
   public short GetShort()
   {
     Position += 2;
     if (Endianness == Endianness.Big)
-      return ByteConverter.ToShort_Big(Source, Position - 2);
-    return ByteConverter.ToShort_Little(Source, Position - 2);
+      return ByteConverter.ToShort_Big(Content, Position - 2);
+    return ByteConverter.ToShort_Little(Content, Position - 2);
   }
 
   public ushort GetUShort()
   {
     Position += 2;
     if (Endianness == Endianness.Big)
-      return ByteConverter.ToUshort_Big(Source, Position - 2);
-    return ByteConverter.ToUshort_Big(Source, Position - 2);
+      return ByteConverter.ToUshort_Big(Content, Position - 2);
+    return ByteConverter.ToUshort_Big(Content, Position - 2);
   }
 
   public int GetInt()
   {
     Position += 4;
     if (Endianness == Endianness.Big)
-      return ByteConverter.ToInt_Big(Source, Position - 4);
-    return ByteConverter.ToInt_Little(Source, Position - 4);
+      return ByteConverter.ToInt_Big(Content, Position - 4);
+    return ByteConverter.ToInt_Little(Content, Position - 4);
   }
 
   public uint GetUInt()
   {
     Position += 4;
     if (Endianness == Endianness.Big)
-      return ByteConverter.ToUint_Big(Source, Position - 4);
-    return ByteConverter.ToUint_Big(Source, Position - 4);
+      return ByteConverter.ToUint_Big(Content, Position - 4);
+    return ByteConverter.ToUint_Big(Content, Position - 4);
   }
 
   public long GetLong()
   {
     Position += 8;
     if (Endianness == Endianness.Big)
-      return ByteConverter.ToLong_Big(Source, Position - 8);
-    return ByteConverter.ToLong_Little(Source, Position - 8);
+      return ByteConverter.ToLong_Big(Content, Position - 8);
+    return ByteConverter.ToLong_Little(Content, Position - 8);
   }
 
   public ulong GetULong()
   {
     Position += 8;
     if (Endianness == Endianness.Big)
-      return ByteConverter.ToUlong_Big(Source, Position - 8);
-    return ByteConverter.ToUlong_Big(Source, Position - 8);
+      return ByteConverter.ToUlong_Big(Content, Position - 8);
+    return ByteConverter.ToUlong_Big(Content, Position - 8);
   }
 
 
   private readonly int Translate(Index index)
   {
-    return index.IsFromEnd ? Source.Length - index.Value : index.Value;
+    return index.IsFromEnd ? Content.Length - index.Value : index.Value;
   }
 
   private readonly (int, int) Translate(Range range)
